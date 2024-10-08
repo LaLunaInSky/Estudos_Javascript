c92771acf742669b8deba9ff86e44252ed54db9b
LaLunaInSky <moonaflordapele@gmail.com>
2024-10-08 10:50:23 -0300

    08/10/2024 10:50

---
+++ Módulo_D/Aulas/aula_011/condições_pt_1.js
@@ -0,0 +1,21 @@
+/*Condições parte 1*/
+
+/*Condição Simples - exemplo -*/
+/*var vel = 90
+console.log(`A velocidade do automovel é de ${vel}km/h.`)
+
+if (vel > 80) {
+    console.log('Você ultrapassou o limite de velocidade desta via! MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')*/
+
+/*Condição Composta - exemplo -*/
+var país = 'Russia'
+console.log(`Vivendo no(a) ${país}`)
+
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro(a)!')
+} else {
+    console.log('Você é Estrangeiro(a)!')
+}
