ff4415d4923439a9020612ce33f555f6bd7ef99e
LaLunaInSky <moonaflordapele@gmail.com>
2024-10-09 15:59:26 -0300

    09/10/2024 15:59

--- Módulo_D/Aulas/aula_012/condições_parte02.js
+++ Módulo_D/Aulas/aula_012/condições_parte02.js
@@ -2,21 +2,30 @@
  
 /*Condições Aninhadas*/
 /*Exemplo 1*/
-var idade = 14
-console.log(`Você tem ${idade} anos.`)
+function escolha() {
+    var escolha = Number(document.querySelector('input#numberIn').value)
+    var resposta = document.querySelector('div#resposta')
 
-if (idade < 18) {
-    if (idade > 15) {
-        console.log('Voto opcional')
-    } else {
-        console.log('Não vota')
-    }
-} else {
-    if (idade > 65) {
-        console.log('Voto opcional')
-    } else {
-        console.log('Vota')
-    }
-}
+    if (escolha == 1) {
+        var idade = 67
+        resposta.innerHTML = `<p>Você tem ${idade} anos.</p>`
 
+        if (idade < 18) {
+            if (idade > 15) {
+                resposta.innerHTML += 'Voto opcional'
+            } else {
+                resposta.innerHTML += 'Não vota'
+            }
+        } else {
+            if (idade > 65) {
+                resposta.innerHTML += 'Voto opcional'
+            } else {
+                resposta.innerHTML += 'Vota'
+            }
+        }
+
+    } else if (escolha == 2) {
 /*Exemplo 2*/
+        resposta.innerHTML = 'exemplo2'
+    }
+} 
