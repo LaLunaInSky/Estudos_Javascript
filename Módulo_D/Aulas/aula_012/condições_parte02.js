f2ab785e2db9659e361116b40fd7a2f682d4e551
LaLunaInSky <moonaflordapele@gmail.com>
2024-10-09 15:44:37 -0300

    09/10/2024 15:44

--- Módulo_D/Aulas/aula_012/condições_parte02.js
+++ Módulo_D/Aulas/aula_012/condições_parte02.js
@@ -1,1 +1,22 @@
 /*Condições Parte 2*/
+ 
+/*Condições Aninhadas*/
+/*Exemplo 1*/
+var idade = 14
+console.log(`Você tem ${idade} anos.`)
+
+if (idade < 18) {
+    if (idade > 15) {
+        console.log('Voto opcional')
+    } else {
+        console.log('Não vota')
+    }
+} else {
+    if (idade > 65) {
+        console.log('Voto opcional')
+    } else {
+        console.log('Vota')
+    }
+}
+
+/*Exemplo 2*/
