8e7da7ff2a3dba37d1445e337b8f820d74845c1e
LaLunaInSky <moonaflordapele@gmail.com>
2024-10-09 16:39:12 -0300

    09/10/2024 16:39

--- Módulo_D/Aulas/aula_012/condições_parte02.js
+++ Módulo_D/Aulas/aula_012/condições_parte02.js
@@ -1,49 +1,85 @@
 /*Condições Parte 2*/
  
 /*Condições Aninhadas*/
-/*Exemplo 1*/
 function escolha() {
+/*Exemplo 1*/
     var escolha = Number(document.querySelector('input#numberIn').value)
     var resposta = document.querySelector('div#resposta')
 
-    if (escolha == 1) {
-        var idade = 67
-        resposta.innerHTML = `<p>Você tem ${idade} anos.</p>`
+    switch(escolha) {
+        case 1:
+            var idade = 67
+            resposta.innerHTML = `<p>Você tem ${idade} anos.</p>`
 
-        if (idade < 18) {
-            if (idade > 15) {
-                resposta.innerHTML += 'Voto opcional'
+            if (idade < 18) {
+                if (idade > 15) {
+                    resposta.innerHTML += 'Voto opcional'
+                } else {
+                    resposta.innerHTML += 'Não vota'
+                }
             } else {
-                resposta.innerHTML += 'Não vota'
+                if (idade > 65) {
+                    resposta.innerHTML += 'Voto opcional'
+                } else {
+                    resposta.innerHTML += 'Vota'
+                }
             }
-        } else {
-            if (idade > 65) {
-                resposta.innerHTML += 'Voto opcional'
-            } else {
-                resposta.innerHTML += 'Vota'
-            }
-        }
-
-    } else if (escolha == 2) {
+            break
+            
+        case 2:
 /*Exemplo 2*/
-        var horaAtual = new Date()
-        var hora = horaAtual.getHours()
-        resposta.innerHTML = `Agora são exatamente ${hora} horas. <br>`
+            var horaAtual = new Date()
+            var hora = horaAtual.getHours()
+            resposta.innerHTML = `Agora são exatamente ${hora} horas. <br>`
 
-        if (hora < 13) {
-            if (hora < 5) {
-                resposta.innerHTML += 'Boa madrugada!'
-            } else {
-                resposta.innerHTML += 'Bom dia!'
+            if (hora < 13) {
+                if (hora < 5) {
+                    resposta.innerHTML += 'Boa madrugada!'
+                } else {
+                    resposta.innerHTML += 'Bom dia!'
+                }
+            } else if (hora < 24){
+                if (hora < 19) {
+                    resposta.innerHTML += 'Boa tarde!'
+                } else {
+                    resposta.innerHTML += 'Boa noite!'
+                }
             }
-        } else if (hora < 24){
-            if (hora < 19) {
-                resposta.innerHTML += 'Boa tarde!'
-            } else {
-                resposta.innerHTML += 'Boa noite!'
-            }
-        }
+            break
+
+        default:
+            resposta.innerHTML = `Erro`
+            break
     }
-}
+}   
 
 /*Condições Múltiplas*/
+var agora = new Date()
+var diaSemana = agora.getDay()
+
+switch(diaSemana) {
+    case 0: 
+        console.log('Hoje é Domingo!')
+        break
+    case 1: 
+        console.log('Hoje é Segunda!')
+        break
+    case 2: 
+        console.log('Hoje é Terça!')
+        break
+    case 3: 
+        console.log('Hoje é Quarta!')
+        break
+    case 4: 
+        console.log('Hoje é Quinta!')
+        break
+    case 5: 
+        console.log('Hoje é Sexta!')
+        break
+    case 6: 
+        console.log('Hoje é Sábado!')
+        break
+    default:
+        console.log('Erro')
+        break
+}
