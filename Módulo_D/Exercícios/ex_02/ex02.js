23d877a239229d9f0c8adf41225ef3a9c2cfe572
LaLunaInSky <moonaflordapele@gmail.com>
2024-10-14 11:06:00 -0300

    14/10/2024 11:05

--- Módulo_D/Exercícios/ex_02/ex02.js
+++ Módulo_D/Exercícios/ex_02/ex02.js
@@ -1,8 +1,37 @@
 function Verificação() {
+    var date = new Date()
+    var anoAtual = date.getFullYear()
     var anoNas = document.querySelector('input#anoDeNascimento').value
     var gêneroM = document.querySelector('input#respostaMas').checked
     var gêneroF = document.querySelector('input#respostaFem').checked
     var resultado  = document.querySelector('section#resultado')
+    var idade = 0
+    var gêneroFinal = ''
+    var validação = [false, false]
+    var imagem = null
 
-    resultado.innerHTML = `<p>${anoNas} | ${gêneroM} | ${gêneroF}</p>`
+    if (anoNas <= anoAtual && anoNas >= (anoAtual - 110)) {
+        idade = anoAtual - Number(anoNas)
+        validação[0] = true
+    }
+
+    if (gêneroF == true || gêneroM == true) {
+        validação[1] = true
+        if (gêneroF == true) {
+            gêneroFinal = 'Mulher'
+        } else if (gêneroM == true) {
+            gêneroFinal = 'Homem'
+        }
+    }
+
+    if (validação[0] == true) {
+
+    }
+
+    if (validação[0] == true && validação[1] == true) {
+        resultado.innerHTML = `<p id='resultadoP'>Detectamos ${gêneroFinal} com ${idade} anos.</p>`
+        resultado.innerHTML += `<img src="${imagem}" alt="imagem rosto de acordo com idade" id='imagemRosto'>`
+    } else {
+        alert('Erro, alguma informação está errado ou faltando!')
+    }
 }
