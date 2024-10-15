2ac52ed70dde7e05f04b39f7afc2deb383fab762
LaLunaInSky <moonaflordapele@gmail.com>
2024-10-15 15:22:17 -0300

    15/10/2024 15:22

--- Módulo_E/Aulas/aula_013/repetições_parte01.js
+++ Módulo_E/Aulas/aula_013/repetições_parte01.js
@@ -1,1 +1,13 @@
 /*Repetições Parte 01*/
+
+/*while*/
+
+var time = 0
+while (time < 60) {
+    var date = new Date()
+    time = date.getSeconds()
+    if (copy_time != time) {
+        console.log(`tudo bem? ${time}`)
+    }  
+    var copy_time = time
+}
