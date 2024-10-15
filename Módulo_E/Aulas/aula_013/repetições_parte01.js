e1035cc9d0e820a146d18f1ecdba200a5f9ec349
LaLunaInSky <moonaflordapele@gmail.com>
2024-10-15 16:08:06 -0300

    15/10/2024 16:08

--- Módulo_E/Aulas/aula_013/repetições_parte01.js
+++ Módulo_E/Aulas/aula_013/repetições_parte01.js
@@ -2,12 +2,30 @@
 
 /*while*/
 
-var time = 0
-while (time < 60) {
-    var date = new Date()
-    time = date.getSeconds()
-    if (copy_time != time) {
-        console.log(`tudo bem? ${time}`)
-    }  
-    var copy_time = time
+function rodarWhile() {
+    var time = 0
+    while (time < 60) {
+        var date = new Date()
+        time = date.getSeconds()
+        if (copy_time != time) {
+            console.log(`tudo bem? ${time}`)
+        }  
+        var copy_time = time
+    }
 }
+
+/*do while*/
+
+function rodarDoWhile() {
+    var time = 0
+    do {
+        var date = new Date()
+        time = date.getSeconds()
+        if (copy_time != time) {
+            console.log(`tudo bem? ${time}`)
+        }  
+        var copy_time = time 
+    } while (time < 60)
+}
+
+rodarWhile()
