835758efc739b54eb74363ab3bd0d77cb84261fc
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-02 09:52:32 -0300

    02/11/2024 09:52

--- Módulo_F/Aulas/aula_15/aula_015.js
+++ Módulo_F/Aulas/aula_15/aula_015.js
@@ -1,5 +1,18 @@
-let num = [5,8,4]
+let num = [5, 8, 2, 9, 3, 7]
 
-for (var c = 0;c<3;c++) {
+num[num.length] = 1 //ou
+num.push(2) //acrescentar na ultima possição
+
+num.sort() //arrumar os valores em ordem crescente
+
+//for normal
+for (var c = 0;c<num.length;c++) {
+    console.log(`O número de agora é o ${num[c]}`)
+}
+
+console.log("")
+
+//for in
+for (let c in num) {
     console.log(num[c])
 }
