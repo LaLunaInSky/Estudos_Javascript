b866f18aaa857f28f0d9e99cde9115caa0aa2da8
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-03 09:29:21 -0300

    03/11/2024 09:29

--- Módulo_F/Aulas/aula_016/aula_016.js
+++ Módulo_F/Aulas/aula_016/aula_016.js
@@ -11,6 +11,16 @@ function Soma(num1=0, num2=0){
     return num1 + num2
 }
 
+function Fatorial(num) {
+    let numero = 1
+    for (let c = num;c>0;c--) {
+        numero = numero * c
+    }
+    return numero
+}
+
+console.log(Fatorial(6))
+
 let v = function(x) {
     return x*2
 }
