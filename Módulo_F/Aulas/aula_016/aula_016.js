ff311d617472d692fb265596ff28811f2a7ebd17
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-03 09:13:08 -0300

    03/11/2024 09:13

--- Módulo_F/Aulas/aula_016/aula_016.js
+++ Módulo_F/Aulas/aula_016/aula_016.js
@@ -7,5 +7,16 @@ function parImpar(num) {
     }
 }
 
-let dado = parImpar(5)
+function Soma(num1=0, num2=0){
+    return num1 + num2
+}
+
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(10))
+
+let dado = Soma(4)
 console.log(dado)
+console.log(parImpar(dado))
