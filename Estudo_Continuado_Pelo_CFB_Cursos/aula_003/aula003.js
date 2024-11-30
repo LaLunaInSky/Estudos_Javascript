acdf8ee54e60a80790201d76ec66d72f22a60754
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-30 08:28:01 -0300

    30/11/2024 08:27

--- Estudo_Continuado_Pelo_CFB_Cursos/aula_003/aula003.js
+++ Estudo_Continuado_Pelo_CFB_Cursos/aula_003/aula003.js
@@ -1,1 +1,21 @@
 "use strict"
+
+//Diferença de escopos de declarar variáveis com let e var
+
+function teste()  {
+    let nome = "Bruno"
+
+    if (true) {
+        var nome2 = 'Julia'
+        console.log(`Dentro do if do testeFunction: ${nome} e ${nome2}`)
+        nome = 'Ronaldo'
+    }
+
+    console.log(`Fora do if, porém dentro do testeFunction: ${nome} e ${nome2}`)
+}
+
+const curso = 'Javascript'
+
+teste() 
+console.log(curso)
+console.log(`Fora do testeFunction: ${nome2} e ${nome}`)
