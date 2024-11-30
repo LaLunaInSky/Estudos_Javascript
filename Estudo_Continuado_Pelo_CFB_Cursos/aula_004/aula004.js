3a76aa1aeebf2ac2a002a5950f4e9e75b1c2b262
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-30 09:10:10 -0300

    30/11/2024 09:10

--- Estudo_Continuado_Pelo_CFB_Cursos/aula_004/aula004.js
+++ Estudo_Continuado_Pelo_CFB_Cursos/aula_004/aula004.js
@@ -3,4 +3,8 @@ let num1=0,num2=0,res=0
 num1=10
 num1/=2
 
+num2=15
+
+res=(num1+num2)*2
+
 console.log(`Num1: ${num1}, Num2: ${num2}, Res: ${res}`)
---
+++ Estudo_Continuado_Pelo_CFB_Cursos/aula_005/aula005.js
@@ -0,0 +1,51 @@
+"use strict"
+let num1=10,num2=5,num3=10,afirmação=String(),res=Boolean()
+
+/* > */
+afirmação = `${num1} > ${num2}`
+res = num1 > num2
+console.log(`${afirmação}? Is ${res}`)
+
+/* < */
+afirmação = `${num1} < ${num2}`
+res = num1 < num2
+console.log(`${afirmação}? Is ${res}`)
+
+/* >= */
+afirmação = `${num1} >= ${num3}`
+res = num1 >= num3
+console.log(`${afirmação}? Is ${res}`)
+
+afirmação = `${num1} >= ${num2}`
+res = num1 >= num2
+console.log(`${afirmação}? Is ${res}`)
+
+
+/* <= */
+afirmação = `${num1} <= ${num3}`
+res = num1 <= num3
+console.log(`${afirmação}? Is ${res}`)
+
+afirmação = `${num1} <= ${num2}`
+res = num1 <= num2
+console.log(`${afirmação}? Is ${res}`)
+
+
+/* == */
+afirmação = `${num1} == ${num3}`
+res = num1 == num3
+console.log(`${afirmação}? Is ${res}`)
+
+afirmação = `${num1} == ${num2}`
+res = num1 == num2
+console.log(`${afirmação}? Is ${res}`)
+
+
+/* != */
+afirmação = `${num1} != ${num3}`
+res = num1 != num3
+console.log(`${afirmação}? Is ${res}`)
+
+afirmação = `${num1} != ${num2}`
+res = num1 != num2
+console.log(`${afirmação}? Is ${res}`)
