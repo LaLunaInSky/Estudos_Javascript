0d63c08600f97ebdc3f818e0d429ea5409a703a0
LaLunaInSky <moonaflordapele@gmail.com>
2024-12-19 13:28:01 -0300

    19/12/2024 13:27

---
+++ Estudo_Continuado_Pelo_CFB_Cursos/aula_020/aula020.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function soma2_10() {
+    let num1 = 2
+    let num2 = 10
+    let soma = num1 + num2
+    console.log(soma)
+}
+
+soma2_10()
---
+++ Estudo_Continuado_Pelo_CFB_Cursos/aula_021/aula021.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function canal() {
+    let num1 = 10
+    let num2 = 2
+    let res = num1 * num2
+    return res
+}
+
+let num = canal()
+console.log(num)
