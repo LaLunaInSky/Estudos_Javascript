137858be646f93a67b378d4b88394024c2e97999
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-07 02:02:20 -0300

    07/01/2025 02:02

---
+++ Estudo_Continuado_Pelo_CFB_Cursos/aula_033/aula033.js
@@ -0,0 +1,10 @@
+const divTodas = document.getElementsByTagName('div')
+const query_todasDivs = [...document.querySelectorAll('div')]
+const query_todosCursos = [...document.querySelectorAll('.curso')]
+const cursosC1 = [...document.querySelectorAll('.c1')]
+const cursosC2 = [...document.querySelectorAll('.c2')]
+
+console.log(query_todosCursos)
+console.log(query_todasDivs)
+console.log(cursosC1)
+console.log(cursosC2)
