6c00a7052f8d15ed8bc42fefc12a6675becc22eb
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-11 12:25:31 -0300

    11/01/2025 12:25

---
+++ Estudo_TypeScript_Pelo_CFB_Cursos/aula_05/aula05.js
@@ -0,0 +1,9 @@
+"use strict";
+let numeros_Aula05 = [20, 30, 40];
+numeros_Aula05.push(50);
+numeros_Aula05.unshift(10);
+numeros_Aula05.pop();
+numeros_Aula05.shift();
+console.log(numeros_Aula05);
+let numeros_ro_Aula05 = [100, 200, 300];
+console.log(numeros_ro_Aula05);
