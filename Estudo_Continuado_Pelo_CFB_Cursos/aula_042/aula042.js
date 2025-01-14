652f4f23b76ac36b539acbb67dee5de5b04998fe
lalunainsky <moonaflordapele@gmail.com>
2025-01-14 13:34:54 -0300

    14/01/2025 13:34

---
+++ Estudo_Continuado_Pelo_CFB_Cursos/aula_042/aula042.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const caixaCursosAdicionados = document.querySelector("#caixaCursosAdicionados")
+const btnAddNovoCurso = document.querySelector("#btnAdicionarNovoCurso")
+const inputNomeCurso = document.querySelector("#nomeCurso")
+
+const criarNovoCurso = (nomeCurso)=>{
+    const nomeDoCurso = nomeCurso
+    const caixaDeCursos = document.querySelector("#caixaCursosAdicionados")    
+    
+    const divCurso = document.createElement("div")
+    divCurso.setAttribute("class", 'curso')
+    divCurso.setAttribute("id", `c${caixaDeCursos.children.length + 1}`)
+
+    const labelCurso = document.createElement("label")
+    labelCurso.setAttribute('for', `cursoC${caixaDeCursos.children.length + 1}`)
+    labelCurso.innerHTML = nomeDoCurso
+
+    const inputRadioCurso = document.createElement("input")
+    inputRadioCurso.setAttribute('type', 'radio')
+    inputRadioCurso.setAttribute('name', 'cursos')
+    inputRadioCurso.setAttribute('id', `cursoC${caixaDeCursos.children.length + 1}`)
+
+    divCurso.appendChild(labelCurso)
+    divCurso.appendChild(inputRadioCurso)
+    caixaDeCursos.appendChild(divCurso)
+}
+
+btnAddNovoCurso.addEventListener('click', ()=>{
+    if (inputNomeCurso.value != '') {
+        criarNovoCurso(inputNomeCurso.value)
+    }
+    
+    inputNomeCurso.value = ''
+})
